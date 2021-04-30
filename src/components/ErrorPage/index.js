b3bfc512a8fef66c8e9df897d0936ed6e4eb374d
aimeric <aimericjjb@gmail.com>
2021-04-30 11:52:05 +0300

    stylization of error page

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Batman from '../../images/batman.png'; 
 
 const ErrorPage = () => {
+
+    const centerH2 = {
+        textAlign: 'center', 
+        marginTop: '50px'
+    }
+    const centerImg = {
+        display: 'block', 
+        margin: '40px'
+    }
     return (
         <div className="quiz-bg">
             <div className="container">
-                ErrorPage
+                <h2 style={centerH2} >Oupps, cette page n'existe pas</h2>
+                <img style={centerImg} src={Batman} alt="error_page" />
             </div>
         </div>
     )
